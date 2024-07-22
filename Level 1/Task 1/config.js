fd9ac883effbc96cbf1522d2e87c78483ab0ad44
Ashish <ashishlodhi5559@gmail.com>
2024-07-22 09:54:26 +0530

    first commit

---
+++ Level 1/Task 1/config.js
@@ -0,0 +1,5 @@
+const config = {
+    DISCORD_WEBHOOK_URL: 'https://discord.com/api/webhooks/1264786414248263752/u172-ug8fJHXa6qgwRg8KuU52RbEj_CSmslCmMzbkINdU67pvUf5rjwL-Y6U9sWiOHGp'
+};
+
+window.config = config;
